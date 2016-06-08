588abab576e95355516a6d8946c7e2d047ed3dd9
laurenthoxhaj <laurenthoxhaj@gmail.com>
2016-06-08 15:09:37 +0200

    even more

--- js/register.js
+++ js/register.js
@@ -9,42 +9,48 @@ var is_not_logged_in = function() {
 }
 
 $(document).ready(function(){
-	
+
 	$("#submit_register").click(function(){
+
 		
-		var data = {
-			name: $("#name").val(),
-			username: $("#username").val(),
-			password: $("#password").val(),
-			email: $("#email").val(),
-			action: "signup"
-		};
-		console.log(data);
-		data = JSON.stringify(data);
-		
-		$.ajax({
-			url: root+"register.php",
-			data: data,
-			type: "POST",
-			success: function(response) {
-				console.log("SUCCESS");
-				
-				if(!response.error) {
-					localStorage.setItem("user", JSON.stringify(response.data));
-					window.location = "login.html";
-				}
-				else {
-					// Error handling
-					$("#register_response").html(response.message).show();
-				}
-			},
-			error: function(response) {
-				console.log("ERROR");
-				console.log(response);
-			}
-		});
-		
+
 	});
+	
+	// $("#submit_register").click(function(){
+	//
+	// 	var data = {
+	// 		name: $("#name").val(),
+	// 		username: $("#username").val(),
+	// 		password: $("#password").val(),
+	// 		email: $("#email").val(),
+	// 		action: "signup"
+	// 	};
+	// 	console.log(data);
+	// 	data = JSON.stringify(data);
+	//
+	// 	$.ajax({
+	// 		url: root+"register.php",
+	// 		data: data,
+	// 		type: "POST",
+	// 		success: function(response) {
+	// 			console.log("SUCCESS");
+	//
+	// 			if(!response.error) {
+	// 				localStorage.setItem("user", JSON.stringify(response.data));
+	// 				window.location = "login.html";
+	// 			}
+	// 			else {
+	// 				// Error handling
+	// 				$("#register_response").html(response.message).show();
+	// 			}
+	// 		},
+	// 		error: function(response) {
+	// 			console.log("ERROR");
+	// 			console.log(response);
+	// 		}
+	// 	});
+	//
+	// });
 	
 	if( is_not_logged_in() ) {
 		console.log("User is not logged in");
