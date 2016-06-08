fbdf0c98c69b2e17aed0a8482bd3ccf55457c034
laurenthoxhaj <laurenthoxhaj@gmail.com>
2016-06-08 16:02:02 +0200

    even even more

--- js/register.js
+++ js/register.js
@@ -8,11 +8,61 @@ var is_not_logged_in = function() {
 	return true;
 }
 
+function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+
 $(document).ready(function(){
 
 	$("#submit_register").click(function(){
 
-		
+		var name = $("#name").val();
+		var username = $("#username").val();
+		var email = $("#email").val();
+		var password = $("#password").val();
+
+
+		var is_ok = true;
+
+		$("#register_response").html("").hide();
+
+		// Check if empty
+		if(name.length < 3) {
+			// name is not ok
+			is_ok = false;
+			$("#register_response").append("<div>Name missing</div>").show();
+			//$("#name").addClass("red_border");
+		}
+		if(username.length < 3) {
+			// username is ok
+			is_ok = false;
+
+			var msg = "Username is too short";
+			if(username.length == 0) {
+				msg = "Username missing";
+			}
+
+			$("#register_response").append("<div>"+msg+"</div>").show();
+		}
+		if(!validateEmail(email)) {
+			// email is ok
+			is_ok = false;
+			$("#register_response").append("<div>Email is invalid</div>").show();
+		}
+		if(password.length < 6) {
+			// password is ok
+			is_ok = false;
+			$("#register_response").append("<div>Password missing</div>").show();
+		}
+
+		if(is_ok) {
+			// register user
+			console.log("Everything is ok");
+		}
+		else {
+			console.log("Errors found");
+		}
 
 	});
 	
